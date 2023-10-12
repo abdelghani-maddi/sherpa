ceb591f348c4c51c15b8ef4f49a2bcc30f2d6cdd
Abdelghani Maddi <36671005+abdelghani-maddi@users.noreply.github.com>
2023-10-12 09:38:51 +0200

    Script final extraxtion sherpa romeo 🥳 🎉

--- data extraction.R
+++ data extraction.R
@@ -2,45 +2,54 @@
 library(httr)
 library(jsonlite)
 library(readxl)
-
+library(tidyverse)
 ## Données Revues
 
 journals_openalex <- read_excel("~/Documents/bdd pubpeer/journals_openalex.xlsx")
 
-journals_openalex$issn[1:10]
+# test <- journals_openalex$issn[1:100]
 
-
-url_cancer <- "https://v2.sherpa.ac.uk/cgi/retrieve_by_id?item-type=publication&api-key=3F5913F6-677E-11EE-8559-EAD6AA1EFE05&format=Json&identifier=cancer"
-
-url_anr <- "https://v2.sherpa.ac.uk/cgi/retrieve_by_id?item-type=funder&api-key=3F5913F6-677E-11EE-8559-EAD6AA1EFE05&format=Json&identifier=30"
-url <- "https://v2.sherpa.ac.uk/cgi/retrieve_by_id?item-type=publication&api-key=3F5913F6-677E-11EE-8559-EAD6AA1EFE05&format=Json&identifier=1431-5890"
-
-# URL de l'API
-url <- "https://v2.sherpa.ac.uk/cgi/retrieve_by_id?item-type=publication&api-key=3F5913F6-677E-11EE-8559-EAD6AA1EFE05&format=Json&identifier=1097-0142"
-# Effectuer la requête GET pour récupérer les données JSON
-response <- GET(url_anr)
-
-# Parser les données JSON directement
-json_data <- fromJSON(content(response, "text", encoding = "ISO-8859-1"))
-
-
-# Créer une liste pour stocker les données extraites.
+# Créer une liste pour stocker les données extraites depuis Sherpa.
 data_list <- list()
 
-# Parcourez la colonne "issn" du dataframe.
-for (issn in journals_openalex$issn[1:100]) {
-  # Construiser l'URL avec l'ISSN actuel.
-  url <- paste0("https://v2.sherpa.ac.uk/cgi/retrieve_by_id?item-type=publication&api-key=3F5913F6-677E-11EE-8559-EAD6AA1EFE05&format=Json&identifier=", issn)
+# Créer un vecteur pour stocker les ISSN ayant des données valides.
+valid_issn <- c()
+
+# Spécifiez le point de départ (l'index à partir duquel vous souhaitez reprendre en cas d'interruption)
+point_depart <- 1
+
+# Boucle pour extraire les données
+for (i in point_depart:length(journals_openalex$issn)) {
+  issn <- journals_openalex$issn[i]
   
-  # Effectuez la requête GET pour récupérer les données JSON.
-  response <- GET(url)
-  # Parsez les données JSON et ajoutez-les à la liste.
-  json_data <- fromJSON(content(response, "text", encoding = "ISO-8859-1"))
-  data_list <- append(data_list, list(json_data))
+  tryCatch({
+    # Construire l'URL avec l'ISSN actuel.
+    url <- paste0("https://v2.sherpa.ac.uk/cgi/retrieve_by_id?item-type=publication&api-key=3F5913F6-677E-11EE-8559-EAD6AA1EFE05&format=Json&identifier=", issn)
+    
+    # Effectuer la requête GET pour récupérer les données JSON.
+    response <- GET(url)
+    
+    # Parser les données JSON.
+    json_data <- fromJSON(content(response, "text", encoding = "ISO-8859-1"))
+    
+    # Vérifier si les données JSON ne sont pas vides et si le champ "items" contient des données.
+    if (!is.null(json_data) && !is.null(json_data[["items"]]) && length(json_data[["items"]]) > 0) {
+      data_list <- append(data_list, list(json_data))
+      valid_issn <- c(valid_issn, issn)
+    } else {
+      cat(paste("Les données sont vides pour ISSN", issn, "\n"))
+    }
+  }, error = function(e) {
+    cat("Erreur lors de la requête pour ISSN", issn, ":", conditionMessage(e), "\n")
+  })
+  
+  # Sauvegarder le point de départ actuel à chaque itération (l'API rejette la requête et cela permet de relancer autormatiquement à chaque fois)
+  point_depart <- i
 }
 
-# La liste data_list contient maintenant les données extraites pour tous les ISSN de votre dataframe.
+# Sauvegarder data_list dans un fichier RDS
+saveRDS(data_list, file = "data_list.rds")
 
-
-
+# Charger data_list depuis le fichier RDS
+revues_sherpa <- readRDS("data_list.rds")
 
