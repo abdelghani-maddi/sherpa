5f9414dccb5335f40239307c42567e1b8bd065dc
Abdelghani Maddi <36671005+abdelghani-maddi@users.noreply.github.com>
2023-11-23 12:03:31 +0100

    xlm extraction

--- road_data.R
+++ road_data.R
@@ -11,49 +11,22 @@ library(openxlsx)
 library(XML)
 library(xml2)
 
-# Charger les bibliothèques
-library(xml2)
-library(dplyr)
 
 # Spécifier le chemin du fichier XML
-chemin_fichier <- "~/Documents/sherpa_romeo_juliette/ROAD.xml"
+xml_file <- "~/Documents/sherpa_romeo_juliette/ROAD.xml"
 
-# Charger le fichier XML
-doc_xml <- read_xml(chemin_fichier)
+# Read the XML file using xmlTreeParse
+xml_doc <- xmlTreeParse(xml_file, useInternalNodes = TRUE)
 
-# Extraire les données des champs datafield
-datafields <- xml2::xml_find_all(doc_xml, "//datafield")
+# Extract data from specific nodes
+record_nodes <- getNodeSet(xml_doc, "//record")
 
-# Initialiser une liste pour stocker les données
-donnees_liste <- list()
-
-# Parcourir chaque datafield
-for (datafield in datafields) {
-  # Extraire les valeurs des sous-champs
-  tag <- xml2::xml_attr(datafield, "tag")
-  ind1 <- xml2::xml_attr(datafield, "ind1")
-  ind2 <- xml2::xml_attr(datafield, "ind2")
+# Loop through each record node
+for (record_node in record_nodes) {
+  leader <- xpathSApply(record_node, "//leader", xmlValue)
+  controlfield_001 <- xpathSApply(record_node, "//controlfield[@tag='001']", xmlValue)
   
-  subfields <- xml2::xml_find_all(datafield, "./subfield")
-  subfield_codes <- xml2::xml_attr(subfields, "code")
-  subfield_values <- xml2::xml_text(subfields)
-  
-  # Créer un dataframe pour chaque datafield
-  datafield_df <- data.frame(
-    tag = tag,
-    ind1 = ind1,
-    ind2 = ind2,
-    subfield_code = subfield_codes,
-    subfield_value = subfield_values,
-    stringsAsFactors = FALSE
-  )
-  
-  # Ajouter le dataframe à la liste
-  donnees_liste[[length(donnees_liste) + 1]] <- datafield_df
+  cat("Leader:", leader, "\n")
+  cat("Controlfield 001:", controlfield_001, "\n")
+  # Add more lines to print other fields as needed
 }
-
-# Concaténer tous les dataframes dans un seul dataframe
-donnees_df <- dplyr::bind_rows(donnees_liste)
-
-# Afficher les premières lignes du dataframe
-head(donnees_df)
