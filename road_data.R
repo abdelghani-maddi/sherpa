4d482fbe7690770deab34076002289f00f1ff47a
Abdelghani Maddi <36671005+abdelghani-maddi@users.noreply.github.com>
2023-11-27 15:08:03 +0100

    ROAD data extraction 🎉🎉🥳

--- road_data.R
+++ road_data.R
@@ -1,5 +1,6 @@
 ###################
 # Analyse data ROAD
+# Description des données : https://www.issn.org/services/online-services/road-the-directory-of-open-access-scholarly-resources/ 
 ###################
 
 # Nettoyage
@@ -15,7 +16,7 @@ library(openxlsx2)
 library(openxlsx)
 library(XML)
 library(xml2)
-
+library(data.table)
 
 
 ### Se limiter à 10 records histoire de tester
@@ -25,6 +26,7 @@ xml_file <- "~/Documents/sherpa_romeo_juliette/ROAD.xml"
 # Read the XML file
 doc <- read_xml(xml_file)
 
+#################
 # Extraire les 10 premiers records
 record_nodes <- xml_find_all(doc, "//record[position() <= 63419]")
 
@@ -33,33 +35,39 @@ data_list <- list()
 
 # Loop à travers chaque élément record
 for (record_node in record_nodes) {
-  # Extraire les valeurs des éléments leader, controlfield, datafield
+  # Extraire les valeurs des éléments leader et iso3_code
   leader <- xml_text(xml_find_first(record_node, ".//leader"))
-  controlfields <- xml_text(xml_find_all(record_node, ".//controlfield"))
+  iso3_code <- xml_text(xml_find_all(record_node, ".//datafield[@tag='044']/subfield[@code='c']"))
   
-  # Loop à travers les datafields
-  datafields <- list()
-  for (datafield_node in xml_find_all(record_node, ".//datafield")) {
-    tag <- xml_attr(datafield_node, "tag")
-    
-    # Extraire les valeurs des subfields
-    subfields <- lapply(xml_children(datafield_node), function(subfield_node) {
-      code <- xml_attr(subfield_node, "code")
-      value <- xml_text(subfield_node)
-      paste0(code, "=", value)
-    })
-    
-    # Combinez les tag et subfields dans une chaîne
-    datafields <- c(datafields, paste0(tag, ":", paste(subfields, collapse = ";")))
+  # Extraire l'ISSN
+  issn_node <- xml_find_first(record_node, ".//controlfield[@tag='001']")
+  issn <- ifelse(!is.na(issn_node), xml_text(issn_node), NA)
+  
+  # Extraire la valeur du champ avec le sous-champ a dans le datafield 981
+  field_981_a <- xml_text(xml_find_all(record_node, ".//datafield[@tag='981' and @ind1='0' and @ind2='1']/subfield[@code='a']"))
+  
+  # Créer une liste pour chaque enregistrement
+  record_data <- list(leader = leader, iso3_code = iso3_code, issn = issn)
+  
+  # Ajouter field_981_a à la liste uniquement s'il est disponible
+  if (length(field_981_a) > 0) {
+    record_data$field_981_a <- field_981_a
   }
-  
-  # Combinez toutes les valeurs dans une liste pour chaque enregistrement
-  record_data <- c("leader" = leader, controlfields, datafields)
   
   # Ajouter la liste à la liste principale
   data_list <- c(data_list, list(record_data))
 }
 
+# Enregistrez la liste dans un fichier
+saveRDS(data_list, "road_data.rds")
+
+# Chargez la liste ultérieurement
+data_list <- readRDS("road_data.rds")
+
 # Convertir la liste en un dataframe avec rbindlist
 df <- rbindlist(data_list, use.names = TRUE, fill = TRUE)
+
+# Utilisez la fonction write.xlsx pour sauvegarder le dataframe en tant que fichier Excel
+write.xlsx(df, "road_data.xlsx", row.names = FALSE)
+
 
