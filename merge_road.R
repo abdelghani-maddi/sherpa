ae701ea25edba79b7b6c6d337903a0e554b7affc
Abdelghani Maddi <36671005+abdelghani-maddi@users.noreply.github.com>
2023-11-29 09:46:44 +0100

    Upset final

--- merge_road.R
+++ merge_road.R
@@ -32,6 +32,11 @@ road_data <- read_excel("road_data.xlsx")
 
 openalex <- read_excel("~/Documents/sherpa_romeo_juliette/journals_openalex.xlsx")
 
+openalex <- openalex %>%
+  select(issn) %>%
+  unique()
+names(openalex) <- "openalex_issn"
+
 ###################
 # Revues DOAJ
 ###################
@@ -45,9 +50,9 @@ names(doaj) <- c("issn", "eissn")
 doaj <- c(doaj$issn, doaj$eissn) %>%
   as.data.frame() %>%
   unique()
-names(doaj) <- "doaj"
+names(doaj) <- "doaj_issn"
 doaj <- doaj %>%
-  filter(!is.na(.$doaj))
+  filter(!is.na(.$doaj_issn))
 
 
 ###################
@@ -74,17 +79,34 @@ scopus <- scopus %>%
 scopus <- scopus %>%
   mutate(scopus = paste0(str_sub(scopus1, 1, 4), "-", str_sub(scopus1, 5))) %>%
   select(-scopus1)
-
+names(scopus) <- "scopus_issn"
 ###################
 # Revues Sherpa Romeo
 ###################
 
 sherpa <- read_excel("~/Documents/sherpa_romeo_juliette/df_all_sherpa.xlsx")
 
+sherpa <- c(sherpa$issn1, sherpa$issn2) %>%
+  as.data.frame() %>%
+  unique()
+
+names(sherpa) <- "sherpa_issn"
+sherpa <- sherpa %>%
+  filter(!is.na(.$sherpa_issn))
+
+
 ###################
 # Revues Top Factor
 ###################
 top_factor <- read_csv("~/Documents/sherpa_romeo_juliette/top-factor.csv")
+
+top_factor <- c(top_factor$Issn, top_factor$Eissn) %>%
+  as.data.frame() %>%
+  unique()
+
+names(top_factor) <- "top_factor_issn"
+top_factor <- top_factor %>%
+  filter(!is.na(.$top_factor_issn))
 
 ###################
 # Revues WoS
@@ -95,6 +117,15 @@ wos_SSCI <- read_csv("~/Documents/sherpa_romeo_juliette/wos-core_SSCI 2023-Novem
 wos_ESCI <- read_csv("~/Documents/sherpa_romeo_juliette/wos-core_ESCI 2023-November-20.csv")
 
 wos <- bind_rows(wos_SCIE, wos_AHCI, wos_SSCI, wos_ESCI)
+
+wos <- c(wos$ISSN, wos$eISSN) %>%
+  as.data.frame() %>%
+  unique()
+
+names(wos) <- "wos_issn"
+wos <- wos %>%
+  filter(!is.na(.$wos_issn))
+
 
 ###################
 # CoalitionS DOAJ
@@ -115,3 +146,33 @@ coalitions_non_doaj <- read_excel("~/Documents/sherpa_romeo_juliette/crowdsource
 # Matching
 ###################################################
 ###################################################
+
+df <- road_data %>%
+  mutate(in_scopus = ifelse(.$issn %in% scopus$scopus_issn, 1, 0),
+         in_wos = ifelse(.$issn %in% wos$wos_issn, 1, 0),
+         in_doaj = ifelse(.$issn %in% doaj$doaj_issn, 1, 0),
+         in_sherpa = ifelse(.$issn %in% sherpa$sherpa_issn, 1, 0),
+         in_openalex = ifelse(.$issn %in% openalex$openalex_issn, 1, 0),
+         in_top_factor = ifelse(.$issn %in% top_factor$top_factor_issn, 1, 0))
+
+write.xlsx(df, "road_data_merged.xlsx")
+
+#####################################################
+library(UpSetR)
+
+m <- df[,c(3, 5:10)] %>%
+  as.data.frame()
+
+names(m) <- c("ISSN","Scopus", "WoS", "DOAJ", "Sherpa", "OpenAlex", "TopFactor")
+
+upset(m, order.by = "freq")
+
+# Créer le graphique UpSet
+upset(m, order.by = "freq", nsets = 7, matrix.color = "#DC267F", 
+      main.bar.color = "#648FFF", sets.bar.color = "#FE6100",
+      point.size = 2,
+      text.scale = 2  # Remove the trailing comma
+)
+
+#####################################################
+
